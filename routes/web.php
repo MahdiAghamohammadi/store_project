462f26be89cfa1853b80e6f1b944cd003c9bb8c1
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-08-07 09:52:43 +0430

    make controller and route for category , make modules view folders

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,19 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-*/
-Route::prefix('admin')->namespace('Admin')->group(function() {
+ */
+Route::prefix('admin')->namespace('Admin')->group(function () {
     Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.home');
+
+    Route::prefix('admin')->namespace('Admin')->group(function () {
+        // Category
+        Route::prefix('category')->group(function () {
+            Route::get('/', [CategoryController::class, 'index'])->name('admin.market.category.index');
+            Route::get('/create', [CategoryController::class, 'create'])->name('admin.market.category.create');
+            Route::post('/store', [CategoryController::class, 'store'])->name('admin.market.category.store');
+            Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('admin.market.category.edit');
+            Route::put('/update/{id}', [CategoryController::class, 'update'])->name('admin.market.category.update');
+            Route::delete('/delete/{id}', [CategoryController::class, 'destroy'])->name('admin.market.category.destroy');
+        });
+    });
 });
