57c1b9202ba20fce2e07137f7109f2021b8db563
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-08-08 14:55:06 +0430

    make category section layout

---
+++ resources/views/admin/market/category/index.blade.php
@@ -0,0 +1,53 @@
+@extends('admin.layouts.master')
+@section('head-tag')
+    <title>Category</title>
+@endsection
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
+        </ol>
+    </nav>
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                {{-- header --}}
+                <section class="main-body-container-header">
+                    <h5>دسته بندی</h5>
+                </section>
+                {{-- button and search inout --}}
+                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
+                    <a href="#" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
+                    <div class="max-width-16-rem">
+                        <input class="form-control form-control-sm form-text" type="text" name="" id="" placeholder="جستجو">
+                    </div>
+                </section>
+                <section class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>نام دسته بندی</th>
+                                <th>دسته والد</th>
+                                <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>نمایشگر</td>
+                                <td>دستگاه الکترونیکی</td>
+                                <td class="width-16-rem text-left">
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit pl-1"></i> ویرایش</a>
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i> حذف</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+        </section>
+    </section>
+@endsection
