3ba64d9b7e9d46a62f255540f25e42be6abaad93
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-08-14 10:50:33 +0430

    make product property section

---
+++ resources/views/admin/market/property/index.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.layouts.master')
+@section('head-tag')
+    <title>فرم کالا</title>
+@endsection
+@section('content')
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> فرم کالا</li>
+        </ol>
+    </nav>
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                {{-- header --}}
+                <section class="main-body-container-header">
+                    <h6>فرم کالا</h6>
+                </section>
+                {{-- button and search inout --}}
+                <section class="pb-2 mt-4 mb-3 d-flex justify-content-between align-items-center border-bottom">
+                    <a href="{{ route('admin.market.property.create') }}" class="btn btn-info btn-sm">ایجاد فرم جدید</a>
+                    <div class="max-width-16-rem">
+                        <input class="form-control form-control-sm form-text" type="text" name="" id="" placeholder="جستجو">
+                    </div>
+                </section>
+                <section class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>عنوان فرم</th>
+                                <th>فرم والد</th>
+                                <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td>نمایشگر</td>
+                                <td>کالا الکترونیکی</td>
+                                <td class="text-left width-22-rem">
+                                    <a href="#" class="btn btn-warning btn-sm"><i class="pl-1 fa fa-edit"></i> ویژگی ها</a>
+                                    <a href="#" class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i> ویرایش</a>
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i> حذف</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </section>
+        </section>
+    </section>
+@endsection
