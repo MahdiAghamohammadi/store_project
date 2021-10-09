5dbb301b4ee7506a6c5eb29c4cbeaea27672c7c8
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-10-09 08:52:11 +0330

    create status input and make ajax for change status

--- resources/views/admin/content/category/index.blade.php
+++ resources/views/admin/content/category/index.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
-            <li class="breadcrumb-item font-size-12"> <a href="#">بخش محتوی</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#">بخش محتوی</a></li>
             <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
         </ol>
     </nav>
@@ -19,60 +19,91 @@
                 </section>
                 {{-- button and search inout --}}
                 <section class="pb-2 mt-4 mb-3 d-flex justify-content-between align-items-center border-bottom">
-                    <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
+                    <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته
+                        بندی</a>
                     <div class="max-width-16-rem">
-                        <input class="form-control form-control-sm form-text" type="text" name="" id="" placeholder="جستجو">
+                        <input class="form-control form-control-sm form-text" type="text" name="" id=""
+                               placeholder="جستجو">
                     </div>
                 </section>
                 <section class="table-responsive">
                     <table class="table table-striped table-hover">
                         <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>نام دسته بندی</th>
-                                <th>توضیحات</th>
-                                <th>اسلاگ</th>
-                                <th>عکس</th>
-                                <th>تگ ها</th>
-                                <th>وضعیت</th>
-                                <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
-                            </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>نام دسته بندی</th>
+                            <th>توضیحات</th>
+                            <th>اسلاگ</th>
+                            <th>عکس</th>
+                            <th>تگ ها</th>
+                            <th>وضعیت</th>
+                            <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            @foreach ($postCategories as $postCategory)
-                                <tr>
-                                    <th>{{ $postCategory->id }}</th>
-                                    <td>{{ $postCategory->name }}</td>
-                                    <td>{{ $postCategory->description }}</td>
-                                    <td>{{ $postCategory->slug }}</td>
-                                    <td><img src="{{ asset($postCategory->image) }}" alt="" width="50px" height="50px">
-                                    </td>
-                                    <td>{{ $postCategory->tags }}</td>
-                                    <td>
-                                        <label>
-                                            <input type="checkbox" @if ($postCategory->status === 1)
-                                            checked
-                            @endif>
-                            </label>
-                            </td>
-                            <td class="text-left width-16-rem">
-                                <a href="{{ route('admin.content.category.edit', $postCategory->id) }}"
-                                    class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
-                                    ویرایش</a>
-                                <form class="d-inline" action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
-                                    method="post">
-                                    @csrf
-                                    {{ method_field('delete') }}
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
-                                        حذف</button>
-                                </form>
-                            </td>
+                        @foreach ($postCategories as $postCategory)
+                            <tr>
+                                <th>{{ $postCategory->id }}</th>
+                                <td>{{ $postCategory->name }}</td>
+                                <td>{{ $postCategory->description }}</td>
+                                <td>{{ $postCategory->slug }}</td>
+                                <td><img src="{{ asset($postCategory->image) }}" alt="" width="50px" height="50px">
+                                </td>
+                                <td>{{ $postCategory->tags }}</td>
+                                <td>
+                                    <label>
+                                        <input id="{{ $postCategory->id }}"
+                                               onchange="changeStatus({{ $postCategory->id }})" type="checkbox"
+                                               data-url="{{ route('admin.content.category.status', $postCategory->id) }}"
+                                               @if ($postCategory->status === 1)
+                                               checked
+                                            @endif>
+                                    </label>
+                                </td>
+                                <td class="text-left width-16-rem">
+                                    <a href="{{ route('admin.content.category.edit', $postCategory->id) }}"
+                                       class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
+                                        ویرایش</a>
+                                    <form class="d-inline"
+                                          action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
+                                          method="post">
+                                        @csrf
+                                        {{ method_field('delete') }}
+                                        <button type="submit" class="btn btn-danger btn-sm"><i
+                                                class="fa fa-trash-alt"></i>
+                                            حذف
+                                        </button>
+                                    </form>
+                                </td>
                             </tr>
-                            @endforeach
+                        @endforeach
                         </tbody>
                     </table>
                 </section>
             </section>
         </section>
     </section>
+@endsection
+@section('script')
+    <script type="text/javascript">
+        function changeStatus(id) {
+            let element = $('#' + id);
+            let url = element.attr('data-url');
+            let elementValue = !element.prop('checked');
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function (response) {
+                    if (response.status) {
+                        if (response.checked)
+                            element.prop('checked', true);
+                        else
+                            element.prop('checked', false);
+                    } else {
+                        element.prop('checked', elementValue);
+                    }
+                }
+            })
+        }
+    </script>
 @endsection
