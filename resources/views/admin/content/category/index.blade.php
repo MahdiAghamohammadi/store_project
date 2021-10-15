74c9dedc2a2d4c9cd20840fefb9efcd332de1ed9
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-10-15 14:27:15 +0330

    show toats alert for change status ajax and edit

--- resources/views/admin/content/category/index.blade.php
+++ resources/views/admin/content/category/index.blade.php
@@ -23,61 +23,59 @@
                     <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته
                         بندی</a>
                     <div class="max-width-16-rem">
-                        <input class="form-control form-control-sm form-text" type="text" name="" id=""
-                               placeholder="جستجو">
+                        <input class="form-control form-control-sm form-text" type="text" name="" id="" placeholder="جستجو">
                     </div>
                 </section>
                 <section class="table-responsive">
                     <table class="table table-striped table-hover">
                         <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>نام دسته بندی</th>
-                            <th>توضیحات</th>
-                            <th>اسلاگ</th>
-                            <th>عکس</th>
-                            <th>تگ ها</th>
-                            <th>وضعیت</th>
-                            <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
-                        </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>نام دسته بندی</th>
+                                <th>توضیحات</th>
+                                <th>اسلاگ</th>
+                                <th>عکس</th>
+                                <th>تگ ها</th>
+                                <th>وضعیت</th>
+                                <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @foreach ($postCategories as $postCategory)
-                            <tr>
-                                <th>{{ $postCategory->id }}</th>
-                                <td>{{ $postCategory->name }}</td>
-                                <td>{{ $postCategory->description }}</td>
-                                <td>{{ $postCategory->slug }}</td>
-                                <td><img src="{{ asset($postCategory->image) }}" alt="" width="50px" height="50px">
-                                </td>
-                                <td>{{ $postCategory->tags }}</td>
-                                <td>
-                                    <label>
-                                        <input id="{{ $postCategory->id }}"
-                                               onchange="changeStatus({{ $postCategory->id }})" type="checkbox"
-                                               data-url="{{ route('admin.content.category.status', $postCategory->id) }}"
-                                               @if ($postCategory->status === 1)
-                                               checked
-                                            @endif>
-                                    </label>
-                                </td>
-                                <td class="text-left width-16-rem">
-                                    <a href="{{ route('admin.content.category.edit', $postCategory->id) }}"
-                                       class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
-                                        ویرایش</a>
-                                    <form class="d-inline"
-                                          action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
-                                          method="post">
-                                        @csrf
-                                        {{ method_field('delete') }}
-                                        <button type="submit" class="btn btn-danger btn-sm"><i
-                                                class="fa fa-trash-alt"></i>
-                                            حذف
-                                        </button>
-                                    </form>
-                                </td>
+                            @foreach ($postCategories as $postCategory)
+                                <tr>
+                                    <th>{{ $postCategory->id }}</th>
+                                    <td>{{ $postCategory->name }}</td>
+                                    <td>{{ $postCategory->description }}</td>
+                                    <td>{{ $postCategory->slug }}</td>
+                                    <td><img src="{{ asset($postCategory->image) }}" alt="" width="50px" height="50px">
+                                    </td>
+                                    <td>{{ $postCategory->tags }}</td>
+                                    <td>
+                                        <label>
+                                            <input id="{{ $postCategory->id }}"
+                                                onchange="changeStatus({{ $postCategory->id }})" type="checkbox"
+                                                data-url="{{ route('admin.content.category.status', $postCategory->id) }}"
+                                                @if ($postCategory->status === 1)
+                                            checked
+                            @endif>
+                            </label>
+                            </td>
+                            <td class="text-left width-16-rem">
+                                <a href="{{ route('admin.content.category.edit', $postCategory->id) }}"
+                                    class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
+                                    ویرایش</a>
+                                <form class="d-inline"
+                                    action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
+                                    method="post">
+                                    @csrf
+                                    {{ method_field('delete') }}
+                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
+                                        حذف
+                                    </button>
+                                </form>
+                            </td>
                             </tr>
-                        @endforeach
+                            @endforeach
                         </tbody>
                     </table>
                 </section>
@@ -94,17 +92,55 @@
             $.ajax({
                 url: url,
                 type: "GET",
-                success: function (response) {
+                success: function(response) {
                     if (response.status) {
-                        if (response.checked)
+                        if (response.checked) {
                             element.prop('checked', true);
-                        else
+                            successToast('دسته بندی مورد نظر با موفقیت فعال شد');
+                        } else {
                             element.prop('checked', false);
+                            successToast('دسته بندی مورد نظر با موفقیت غیر فعال شد');
+                        }
                     } else {
                         element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی رخ داده است');
                     }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد');
                 }
-            })
+            });
+
+            function successToast(msg) {
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-success">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
+
+            function errorToast(msg) {
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-danger">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
         }
     </script>
 @endsection
