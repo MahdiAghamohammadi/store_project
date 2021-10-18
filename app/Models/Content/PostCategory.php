84d26236c34340865a8cffc473efe13c3c8ef89a
mahdi <aghamohammadi.mehdi20@gmail.com>
2021-10-18 20:04:05 +0330

    install sluggable package and config for Persian

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -2,12 +2,21 @@
 
 namespace App\Models\Content;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ],
+        ];
+    }
     protected $fillable = ['name', 'description', 'slug', 'image', 'status', 'tags'];
 }
