6db0bd44bb84ebf757e6af8f29dd9639ae52d970
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-01-05 22:27:01 +0330

    make CRUD Page section

--- resources/views/admin/content/page/edit.blade.php
+++ resources/views/admin/content/page/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.master')
 @section('head-tag')
-    <title>ایجاد پیج جدید</title>
+    <title>ویرایش پیج جدید</title>
 @endsection
 @section('content')
     <nav aria-label="breadcrumb">
@@ -8,7 +8,7 @@
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">بخش محتوی</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">پیج ساز</a></li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد پیج جدید</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش پیج جدید</li>
         </ol>
     </nav>
     <section class="row">
@@ -16,20 +16,22 @@
             <section class="main-body-container">
                 {{-- header --}}
                 <section class="main-body-container-header">
-                    <h6>ایجاد پیج جدید</h6>
+                    <h6>ویرایش پیج جدید</h6>
                 </section>
                 {{-- button and search inout --}}
                 <section class="pb-2 mt-4 mb-3 d-flex justify-content-between align-items-center border-bottom">
                     <a href="{{ route('admin.content.page.index') }}" class="btn btn-info btn-sm">بازگشت</a>
                 </section>
                 <section>
-                    <form action="{{ route('admin.content.page.store') }}" method="POST" id="form">
+                    <form action="{{ route('admin.content.page.update', $page->id) }}" method="POST" id="form">
                         @csrf
+                        {{ method_field('put') }}
                         <section class="row">
                             <section class="my-2 col-12 col-md-6">
                                 <div class="form-group">
                                     <label for="title">عنوان</label>
-                                    <input type="text" name="title" id="title" class="form-control form-control-sm" value="{{ old('title') }}">
+                                    <input type="text" name="title" id="title" class="form-control form-control-sm"
+                                        value="{{ old('title', $page->title) }}">
                                 </div>
                                 @error('title')
                                     <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
@@ -43,8 +45,8 @@
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select name="status" id="status" class="form-control form-control-sm">
-                                        <option value="0" @if (old('status') == 0) selected @endif>غیرفعال</option>
-                                        <option value="1" @if (old('status') == 1) selected @endif>فعال</option>
+                                        <option value="0" @if (old('status', $page->status) == 0) selected @endif>غیرفعال</option>
+                                        <option value="1" @if (old('status', $page->status) == 1) selected @endif>فعال</option>
                                     </select>
                                 </div>
                                 @error('status')
@@ -59,7 +61,7 @@
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
                                     <input type="hidden" name="tags" id="tags" class="form-control form-control-sm"
-                                        value="{{ old('tags') }}">
+                                        value="{{ old('tags', $page->tags) }}">
                                     <select class="select2 form-control form-control-sm" id="select_tags" multiple>
 
                                     </select>
@@ -76,7 +78,7 @@
                                 <div class="form-group">
                                     <label for="">محتوی</label>
                                     <textarea name="body" id="body" rows="10"
-                                        class="form-control form-control-sm">{{ old('body') }}</textarea>
+                                        class="form-control form-control-sm">{{ old('body', $page->body) }}</textarea>
                                 </div>
                                 @error('body')
                                     <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
--- resources/views/admin/content/page/index.blade.php
+++ resources/views/admin/content/page/index.blade.php
@@ -18,7 +18,7 @@
                     <h6>پیج ساز</h6>
                 </section>
                 {{-- button and search inout --}}
-                <section class="d-flex justify-content-between align-items-center mt-4 mb-3 pb-2 border-bottom">
+                <section class="pb-2 mt-4 mb-3 d-flex justify-content-between align-items-center border-bottom">
                     <a href="{{ route('admin.content.page.create') }}" class="btn btn-info btn-sm">ایجاد پیج جدید</a>
                     <div class="max-width-16-rem">
                         <input class="form-control form-control-sm form-text" type="text" name="" id="" placeholder="جستجو">
@@ -30,25 +30,111 @@
                             <tr>
                                 <th>#</th>
                                 <th>عنوان</th>
-                                <th>آدرس پیج</th>
-                                <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                                <th>اسلاگ</th>
+                                <th>تگ ها</th>
+                                <th>وضعیت</th>
+                                <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>درباره ما</td>
-                                <td>about</td>
-                                <td class="width-16-rem text-left">
-                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit pl-1"></i> ویرایش</a>
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
+                            @foreach ($pages as $key => $page)
+                                <tr>
+                                    <th>{{ $key += 1 }}</th>
+                                    <td>{{ $page->title }}</td>
+                                    <td>{{ $page->slug }}</td>
+                                    <td>{{ $page->tags }}</td>
+                                    <td>
+                                        <label>
+                                            <input id="{{ $page->id }}" onchange="changeStatus({{ $page->id }})"
+                                                type="checkbox"
+                                                data-url="{{ route('admin.content.page.status', $page->id) }}"
+                                                @if ($page->status === 1)
+                                            checked
+                            @endif>
+                            </label>
+                            </td>
+                            <td class="text-left width-16-rem">
+                                <a href="{{ route('admin.content.page.edit', $page->id) }}"
+                                    class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
+                                    ویرایش</a>
+                                <form class="d-inline" action="{{ route('admin.content.page.destroy', $page->id) }}"
+                                    method="POST">
+                                    @csrf
+                                    {{ method_field('delete') }}
+                                    <button type="submit" class="btn delete btn-danger btn-sm"><i
+                                            class="fa fa-trash-alt"></i>
                                         حذف</button>
-                                </td>
+                                </form>
+                            </td>
                             </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </section>
             </section>
         </section>
     </section>
+@endsection
+@section('script')
+    {{-- this is for change status --}}
+    <script type="text/javascript">
+        function changeStatus(id) {
+            let element = $('#' + id);
+            let url = element.attr('data-url');
+            let elementValue = !element.prop('checked');
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true);
+                            successToast('صفحه مورد نظر با موفقیت فعال شد');
+                        } else {
+                            element.prop('checked', false);
+                            successToast('صفحه مورد نظر با موفقیت غیر فعال شد');
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی رخ داده است');
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد');
+                }
+            });
+
+            function successToast(msg) {
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-success">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
+
+            function errorToast(msg) {
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-danger">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
+        }
+    </script>
+    @include('admin.alerts.sweetalerts.delete-confirm', ['className' => 'delete'])
 @endsection
