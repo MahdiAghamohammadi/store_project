79fec8d2b6e46334f1ec2375b1aedfc54fc87814
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-02-06 22:02:33 +0330

    Brand section created

--- resources/views/admin/market/brand/edit.blade.php
+++ resources/views/admin/market/brand/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.master')
 @section('head-tag')
-    <title>ایجاد برند</title>
+    <title>ویرایش برند</title>
 @endsection
 @section('content')
     <nav aria-label="breadcrumb">
@@ -8,7 +8,7 @@
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">برند ها</a></li>
-            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد برند</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش برند</li>
         </ol>
     </nav>
     <section class="row">
@@ -16,22 +16,24 @@
             <section class="main-body-container">
                 {{-- header --}}
                 <section class="main-body-container-header">
-                    <h6>ایجاد برند</h6>
+                    <h6>ویرایش برند</h6>
                 </section>
                 {{-- button and search inout --}}
                 <section class="pb-2 mt-4 mb-3 d-flex justify-content-between align-items-center border-bottom">
                     <a href="{{ route('admin.market.brand.index') }}" class="btn btn-info btn-sm">بازگشت</a>
                 </section>
                 <section>
-                    <form id="form" action="{{ route('admin.market.brand.store') }}" method="POST"
+                    <form id="form" action="{{ route('admin.market.brand.update', $brand->id) }}" method="POST"
                         enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <section class="row">
                             <section class="my-2 col-12 col-md-6">
                                 <div class="form-group">
                                     <label for="original_name">نام اصلی برند</label>
                                     <input type="text" name="original_name" id="original_name"
-                                        class="form-control form-control-sm" value="{{ old('original_name') }}">
+                                        class="form-control form-control-sm"
+                                        value="{{ old('original_name', $brand->original_name) }}">
                                 </div>
                                 @error('original_name')
                                     <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
@@ -45,7 +47,8 @@
                                 <div class="form-group">
                                     <label for="persian_name">نام فارسی برند</label>
                                     <input type="text" name="persian_name" id="persian_name"
-                                        class="form-control form-control-sm" value="{{ old('persian_name') }}">
+                                        class="form-control form-control-sm"
+                                        value="{{ old('persian_name', $brand->persian_name) }}">
                                 </div>
                                 @error('persian_name')
                                     <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
@@ -59,7 +62,7 @@
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
                                     <input type="hidden" name="tags" id="tags" class="form-control form-control-sm"
-                                        value="{{ old('tags') }}">
+                                        value="{{ old('tags', $brand->tags) }}">
                                     <select class="select2 form-control form-control-sm" id="select_tags" multiple>
 
                                     </select>
@@ -76,8 +79,8 @@
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select name="status" id="status" class="form-control form-control-sm">
-                                        <option value="0" @if (old('status') == 0) selected @endif>غیرفعال</option>
-                                        <option value="1" @if (old('status') == 1) selected @endif>فعال</option>
+                                        <option value="0" @if (old('status', $brand->status) == 0) selected @endif>غیرفعال</option>
+                                        <option value="1" @if (old('status', $brand->status) == 1) selected @endif>فعال</option>
                                     </select>
                                 </div>
                                 @error('status')
@@ -100,11 +103,29 @@
                                         </strong>
                                     </span>
                                 @enderror
+                                <section class="row">
+                                    @php
+                                        $number = 1;
+                                    @endphp
+                                    @foreach ($brand->logo['indexArray'] as $key => $value)
+                                        <section class="col-md-{{ 6 / $number }}">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" name="currentImage"
+                                                    id="{{ $number }}" value="{{ $key }}"
+                                                    @if ($brand->logo['currentImage'] == $key) checked @endif>
+                                                <label for="{{ $number }}" class="mx-2 form-check-label"><img
+                                                        src="{{ asset($value) }}" class="w-100"></label>
+                                            </div>
+                                        </section>
+                                        @php
+                                            $number++;
+                                        @endphp
+                                    @endforeach
+                                </section>
+                                <section class="mt-2 col-12">
+                                    <button type="submit" class="btn btn-primary btn-sm">ثبت</button>
+                                </section>
                             </section>
-                            <section class="col-12">
-                                <button type="submit" class="btn btn-primary btn-sm">ثبت</button>
-                            </section>
-                        </section>
                     </form>
                 </section>
             </section>
--- resources/views/admin/market/brand/index.blade.php
+++ resources/views/admin/market/brand/index.blade.php
@@ -29,26 +29,112 @@
                         <thead>
                             <tr>
                                 <th>#</th>
-                                <th>نام برند</th>
+                                <th>نام فارسی برند</th>
+                                <th>نام انگلیسی برند</th>
                                 <th>لوگو</th>
+                                <th>وضعیت</th>
                                 <th class="text-center max-width-16-rem"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>اپل</td>
-                                <td><img src="logo.png" alt="apple" class="max-height-2rem"></td>
-                                <td class="text-left width-16-rem">
-                                    <a href="#" class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i> ویرایش</a>
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
+                            @foreach ($brands as $key => $brand)
+                                <tr>
+                                    <th>{{ $key += 1 }}</th>
+                                    <td>{{ $brand->persian_name }}</td>
+                                    <td>{{ $brand->original_name }}</td>
+                                    <td>
+                                        <img src="{{ asset($brand->logo['indexArray'][$brand->logo['currentImage']]) }}"
+                                            alt="" width="50px" height="50px">
+                                    </td>
+                                    <td>
+                                        <label>
+                                            <input id="{{ $brand->id }}" onchange="changeStatus({{ $brand->id }})"
+                                                type="checkbox"
+                                                data-url="{{ route('admin.market.brand.status', $brand->id) }}"
+                                                @if ($brand->status === 1)
+                                            checked
+                            @endif>
+                            </label>
+                            </td>
+                            <td class="text-left width-16-rem">
+                                <a href="{{ route('admin.market.brand.edit', $brand->id) }}"
+                                    class="btn btn-primary btn-sm"><i class="pl-1 fa fa-edit"></i>
+                                    ویرایش</a>
+                                <form class="d-inline" action="{{ route('admin.market.brand.destroy', $brand->id) }}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn delete btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
                                         حذف</button>
-                                </td>
+                                </form>
+                            </td>
                             </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </section>
             </section>
         </section>
     </section>
+@endsection
+@section('script')
+    <script type="text/javascript">
+        function changeStatus(id) {
+            let element = $('#' + id);
+            let url = element.attr('data-url');
+            let elementValue = !element.prop('checked');
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true);
+                            successToast('برند مورد نظر با موفقیت فعال شد');
+                        } else {
+                            element.prop('checked', false);
+                            successToast('برند مورد نظر با موفقیت غیر فعال شد');
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی رخ داده است');
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد');
+                }
+            });
+
+            function successToast(msg) {
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-success">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
+
+            function errorToast(msg) {
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="py-3 text-white toast-body d-flex bg-danger">\n' +
+                    '<strong class="ml-auto text-right ">' + msg + '</strong>\n' +
+                    '<button type="submit" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5000).queue(function() {
+                    $(this).remove();
+                });
+            }
+        }
+    </script>
+    @include('admin.alerts.sweetalerts.delete-confirm', ['className' => 'delete'])
 @endsection
