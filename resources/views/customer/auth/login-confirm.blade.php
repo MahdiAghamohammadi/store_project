d3a18c25acb402f077e99bb127c9e9baae611311
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-04-20 21:20:38 +0430

    add timer to resend otp code

--- resources/views/customer/auth/login-confirm.blade.php
+++ resources/views/customer/auth/login-confirm.blade.php
@@ -1,6 +1,11 @@
 @extends('customer.layouts.master-simple')
 @section('head-tag')
     <title>تایید عضویت</title>
+    <style>
+        #resend-otp {
+            font-size: 1rem;
+        }
+    </style>
 @endsection
 @section('content')
     <section class="pb-5 vh-100 d-flex justify-content-center align-items-center">
@@ -29,7 +34,7 @@
                     <input type="text" name="otp" value="{{ old('otp') }}">
                     <section class="my-2">
                         @error('otp')
-                            <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
+                        <span class="p-1 text-white rounded alert_required bg-danger" role="alert">
                                 <span>
                                     {{ $message }}
                                 </span>
@@ -38,11 +43,41 @@
                     </section>
                 </section>
                 <section class="login-btn d-grid g-2">
-                    <button class="btn btn-danger">ورود به آمازون</button>
+                    <button class="btn btn-danger">تایید</button>
                 </section>
-                <section class="login-terms-and-conditions"><a href="#">شرایط و قوانین</a> را خوانده ام و پذیرفته ام
+                <section id="resend-otp" class="d-none">
+                    <a href="#" class="text-decoration-none text-primary">ارسال مجدد کد تایید</a>
                 </section>
+                <section id="timer"></section>
             </section>
         </form>
     </section>
+@endsection
+@section('script')
+    @php
+        $timer = ((new \Carbon\Carbon($otp->created_at))->addMinute(5)->timestamp - \Carbon\Carbon::now()->timestamp) * 1000;
+    @endphp
+    <script>
+        let countDownDate = new Date().getTime() + {{ $timer }};
+        let timer = $('#timer');
+        let resendOtp = $('#resend-otp');
+
+        let x = setInterval(function () {
+            let now = new Date().getTime();
+            let distance = countDownDate - now;
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (minutes == 0) {
+                timer.html(`ارسال مجدد کد تایید تا ${seconds} ثانیه دیگر `);
+            } else {
+                timer.html(`ارسال مجدد کد تایید تا ${minutes} دقیقه و ${seconds} ثانیه دیگر`);
+            }
+            if (distance < 0) {
+                clearInterval(x);
+                timer.addClass('d-none');
+                resendOtp.removeClass('d-none');
+            }
+        }, 1000);
+    </script>
 @endsection
