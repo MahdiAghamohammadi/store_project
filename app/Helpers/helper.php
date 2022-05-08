e5924382e47a74aff0fc572fa1ec89f8df9ffafc
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-05-08 17:42:26 +0430

    add convert numbers and format price helpers

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -6,3 +6,26 @@ function jalaliDate($date, $format = '%A, %d %B %Y')
 {
     return Jalalian::forge($date)->format($format);
 }
+
+function convertPersianToEnglish($number)
+{
+    $pNumber = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    $eNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $number = str_replace($pNumber, $eNumber, $number);
+    return $number;
+}
+
+function convertEnglishToPersian($number)
+{
+    $pNumber = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    $eNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $number = str_replace($eNumber, $pNumber, $number);
+    return $number;
+}
+
+function priceFormat($price)
+{
+    $price = number_format($price, 0, '/', '،');
+    $price = convertEnglishToPersian($price);
+    return $price;
+}
