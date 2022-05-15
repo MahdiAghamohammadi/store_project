a8f374ae4bd27a791bbab475348ec7c17773ab56
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-05-15 19:48:25 +0430

    make dynamically the rest of the home page

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -15,10 +15,10 @@ class HomeController extends Controller
         $slideShowImages = Banner::where('position', 0)->where('status', 1)->get();
         $topBanners = Banner::where('position', 1)->where('status', 1)->take(2)->get();
         $middleBanners = Banner::where('position', 2)->where('status', 1)->take(2)->get();
-        $bottomBanners = Banner::where('position', 3)->where('status', 1)->first();
+        $bottomBanner = Banner::where('position', 3)->where('status', 1)->first();
         $brands = Brand::all();
         $mostVisitedProducts = Product::latest()->take(10)->get();
         $offerProducts = Product::latest()->take(10)->get();
-        return view('customer.home', compact('slideShowImages', 'topBanners', 'middleBanners', 'bottomBanners', 'brands', 'mostVisitedProducts', 'offerProducts'));
+        return view('customer.home', compact('slideShowImages', 'topBanners', 'middleBanners', 'bottomBanner', 'brands', 'mostVisitedProducts', 'offerProducts'));
     }
 }
