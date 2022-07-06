75e5550d99e16655769c861003737194fa36403c
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-07-06 20:23:53 +0430

    show cartItems in header for authenticated users

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Content\Comment;
+use App\Models\Market\CartItem;
 use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +30,14 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('admin.layouts.header', function ($view) {
             $view->with('unseenComments', Comment::where('seen', 0)->get());
             $view->with('notifications', Notification::where('read_at', null)->get());
+        });
+
+        view()->composer('customer.layouts.header', function ($view) {
+            if (Auth::check()) {
+                $user_id = Auth::user()->id;
+                $cartItems = CartItem::where('user_id', $user_id)->get();
+                $view->with('cartItems', $cartItems);
+            }
         });
     }
 }
