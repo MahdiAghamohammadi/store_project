e4e6abdf424a36eaf43c0d69f6499e812f846440
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-07-21 13:07:46 +0430

    create rule for national code and refactor code

--- app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
+++ app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Customer\SalesProcess;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\SalesProcess\ProfileCompletionRequest;
 use App\Models\Market\CartItem;
-use Illuminate\Http\Request;
 
 class ProfileCompletionController extends Controller
 {
@@ -16,19 +16,42 @@ class ProfileCompletionController extends Controller
         return view('customer.sales-process.profile-completion', compact('user', 'cartItems'));
     }
 
-    public function update(Request $request)
+    public function update(ProfileCompletionRequest $request)
     {
-        $request->validate([
-            'first_name' => 'sometimes|required',
-            'last_name' => 'sometimes|required',
-            'email' => 'email|unique:users,email',
-            'mobile' => 'sometimes|required|min:10|max:13||unique:users,mobile',
-            'national_code' => 'sometimes|required',
-        ]);
 
         $user = auth()->user();
-        $inputs = $request->all();
-        $user->update($inputs);
-        return redirect()->route('customer.sales-process.address-and-delivery');
+        $national_code = convertPersianToEnglish($request->national_code);
+        $inputs = [
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'national_code' => $request->national_code,
+        ];
+        if (isset($request->mobile) && empty($user->mobile)) {
+            $mobile = convertPersianToEnglish($request->mobile);
+
+            if (preg_match('/^(\+98|98|0)9\d{9}$/', $mobile)) {
+                $type = 0; // 0 => mobile
+
+                $mobile = ltrim($mobile, '0');
+                $mobile = substr($mobile, 0, 2) == '98' ? substr($mobile, 2) : $mobile;
+                $mobile = str_replace('+98', '', $mobile);
+
+                $inputs['mobile'] = $mobile;
+            } else {
+                $errorText = 'فرمت شماره موبایل معتبر نیست.';
+                return redirect()->back()->withErrors(['mobile', $errorText]);
+            }
+        }
+        if (isset($request->email) && empty($user->email)) {
+            $email = convertPersianToEnglish($request->email);
+            $inputs['email'] = $email;
+        }
+
+        $inputs = array_filter($inputs);
+
+        if (!empty($inputs)) {
+            $user->update($inputs);
+            return redirect()->route('customer.sales-process.address-and-delivery');
+        }
     }
 }
